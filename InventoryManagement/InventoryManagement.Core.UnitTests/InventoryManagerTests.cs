a953e4bf98f85177933dd69924b055a3f3e14fe1
Leszek <leszek.pietrzak@gmail.com>
2021-02-06 15:35:52 +0000

    Add InventoryManager

---
+++ InventoryManagement/InventoryManagement.Core.UnitTests/InventoryManagerTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace InventoryManagement.Core.UnitTests
+{
+    [TestFixture]
+    public class InventoryManagerTests
+    {     
+        [Test]
+        public void Test1()
+        {
+            var sut = new InventoryManager();
+
+            Assert.Pass();
+        }
+    }
+}
