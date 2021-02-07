925f469744b70df68f0d9d675e98aae0dba46e3c
Leszek <leszek.pietrzak@gmail.com>
2021-02-07 18:40:42 +0000

    Refactor to support list of items.

--- InventoryManagement/InventoryManagement.Core.UnitTests/InventoryManagerTests.cs
+++ InventoryManagement/InventoryManagement.Core.UnitTests/InventoryManagerTests.cs
@@ -1,10 +1,12 @@
 using NUnit.Framework;
 
+using System;
+
 namespace InventoryManagement.Core.UnitTests
 {
     [TestFixture]
     public class InventoryManagerTests
-    {     
+    {
         [Test]
         public void GivenSoap_ReturnsNotChangedSoap()
         {
@@ -27,6 +29,20 @@ namespace InventoryManagement.Core.UnitTests
             var result = sut.UpdateInventory(listOfItems);
 
             Assert.That(result, Is.EqualTo("NO SUCH ITEM"));
+        }
+
+        [Test]
+        public void GivenListOfSoapAndInvalidItem_ReturnsListOfSoapAndNoSuchItem()
+        {
+            var sut = new InventoryManager();
+
+            var listOfItems = "Soap 2 2" + Environment.NewLine + "Invalid Item 2 2";
+
+            var result = sut.UpdateInventory(listOfItems);
+
+            var lines = result.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            Assert.That(lines[0], Is.EqualTo("Soap 2 2"));
+            Assert.That(lines[1], Is.EqualTo("NO SUCH ITEM"));
         }
     }
 }
--- InventoryManagement/InventoryManagement.Core/InventoryManager.cs
+++ InventoryManagement/InventoryManagement.Core/InventoryManager.cs
@@ -1,11 +1,29 @@
-﻿namespace InventoryManagement.Core
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InventoryManagement.Core
 {
     public class InventoryManager
     {
         public string UpdateInventory(string listOfItems)
         {
-            if (!listOfItems.Contains("Soap")) return "NO SUCH ITEM";
-            return listOfItems;
+            var items = listOfItems.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var updatedItems = UpdateItems(items);
+
+            return string.Join(Environment.NewLine, updatedItems);
+        }
+
+        private IEnumerable<string> UpdateItems(string[] items)
+        {
+            return items.Select(UpdateItem);
+        }
+
+        private string UpdateItem(string item)
+        {
+            if (!item.Contains("Soap")) return "NO SUCH ITEM";
+            return item;
         }
     }
 }
